8358b1ea690ed64ba3768aec053475a7649d3548
dschrimpsher <dan.schrimpsher@gmail.com>
2021-07-18 14:17:36 -0500

    Added 2021 trips

--- public_html/img/2021/meta.js
+++ public_html/img/2021/meta.js
@@ -3,7 +3,32 @@ var campouts = [
     {
         name: "Oak Mountain Biking Trip",
         date: "Jan 29-31"
-    }
+    },
+    {
+        name: "Joe Wheeler Mountain Biking Trip",
+        date: "February 21"
+    },
+    {
+        name: "Bethel Springs Trail Hike",
+        date: "April 3"
+    },
+    {
+        name: "Talakto Spring Camporee",
+        date: "April 16-18"
+    },
+    {
+        name: "Seabase: Florida Keys Sailing & Scuba Adventure",
+        date: "May 31-Jun 6"
+    },
+    {
+        name: "Skymont Scout Reservation: Summer Camp",
+        date: "June 20-26"
+    },
+
+    {
+        name: "Grimes Canoe Base",
+        date: "May 21-23"
+    },
 ];
 
 var items = [
@@ -108,5 +133,60 @@ var items = [
         "w": 1024,
         "h": 576,
         "title": "Oak Mountain Biking Trip"
+    },
+    {
+        "src": "./img/2021/joe_wheeler/20210221_144354.jpg",
+        "w": 1024,
+        "h": 498,
+        "title": "Joe Wheeler Mountain Biking Trip"
+    },
+    {
+        "src": "./img/2021/joe_wheeler/20210221_144628.jpg",
+        "w": 1024,
+        "h": 498,
+        "title": "Joe Wheeler Mountain Biking Trip"
+    },
+    {
+        "src": "./img/2021/joe_wheeler/20210221_151026.jpg",
+        "w": 1024,
+        "h": 498,
+        "title": "Joe Wheeler Mountain Biking Trip"
+    },
+    {
+        "src": "./img/2021/beth_springs/20210403_111240.jpg",
+        "w": 427,
+        "h": 879,
+        "title": "Bethel Springs Trail"
+    },
+    {
+        "src": "./img/2021/beth_springs/20210403_111213.jpg",
+        "w": 1024,
+        "h": 468,
+        "title": "Bethel Springs Trail"
+    }
+    ,
+    {
+        "src": "./img/2021/beth_springs/20210403_111220.jpg",
+        "w": 1024,
+        "h": 468,
+        "title": "Bethel Springs Trail"
+    },
+    {
+        "src": "./img/2021/camporee/20210417_095943.jpg",
+        "w": 1024,
+        "h": 468,
+        "title": "Talakto Spring Camporee"
+    },
+    {
+        "src": "./img/2021/camporee/20210417_110926.jpg",
+        "w": 1024,
+        "h": 468,
+        "title": "Talakto Spring Camporee"
+    },
+    {
+        "src": "./img/2021/camporee/20210417_133553.jpg",
+        "w": 1024,
+        "h": 468,
+        "title": "Talakto Spring Camporee"
     }
 ];
